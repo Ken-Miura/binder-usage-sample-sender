652a199cca8c0617063371d5284267581a5eb03c
kmiura <ken.miura1102@gmail.com>
2019-06-13 23:44:50 +0900

    Succeeded sending long and string

--- app/src/main/java/com/example/bindersender/SenderService.java
+++ app/src/main/java/com/example/bindersender/SenderService.java
@@ -4,7 +4,11 @@ import android.app.Service;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 public final class SenderService extends Service {
@@ -12,16 +16,48 @@ public final class SenderService extends Service {
     private static final String TAG = SenderService.class.getCanonicalName();
     private static final String DEST_PACKAGE_NAME = "com.example.binderreceiver";
     private static final String DEST_CLASS_NAME = "ReceiverService";
+    private static final int MSG_FROM_SENDER_SERVICE = 1;
 
-    private final ServiceConnection merviceConnection = new ServiceConnection() {
+    private final Object lock = new Object();
+    private Messenger mMessenger = null;
+
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             Log.i(TAG, "onServiceConnected");
+            synchronized (lock) {
+                mMessenger = new Messenger(service);
+            }
+            final Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    final Message msg = Message.obtain(null, MSG_FROM_SENDER_SERVICE, 0, 0);
+                    final Bundle bundle = new Bundle();
+                    bundle.putLong("long", 23L);
+                    bundle.putString("str", "!!!String!!!");
+                    msg.setData(bundle);
+                    try {
+                        synchronized (lock) {
+                            if (mMessenger == null) {
+                                Log.w(TAG, "mMessenger == null");
+                                return;
+                            }
+                            mMessenger.send(msg);
+                        }
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            new Thread(r).start();
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
             Log.i(TAG, "onServiceDisconnected");
+            synchronized (lock) {
+                mMessenger = null;
+            }
         }
     };
 
@@ -37,7 +73,7 @@ public final class SenderService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         final Intent i = new Intent();
         i.setClassName(DEST_PACKAGE_NAME, DEST_PACKAGE_NAME + "." + DEST_CLASS_NAME);
-        bindService(i, merviceConnection, Service.BIND_AUTO_CREATE);
+        bindService(i, mServiceConnection, Service.BIND_AUTO_CREATE);
         return Service.START_STICKY;
     }
 }
